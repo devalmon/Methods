9d3d4dcbd65ea95ee3a48de55dca9ef115a6663b
devalmon <phlood@yandex.ru>
2020-05-17 13:31:09 +0300

    Initial Commit

---
+++ methods/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  methods
+//
+//  Created by Alexey Baryshnikov on 17.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ methods/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  methods
+//
+//  Created by Alexey Baryshnikov on 17.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
